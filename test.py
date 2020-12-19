05c0cdef1992f11e6b146ee01e48f7a33accbc91
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2020-12-19 16:42:23 +0000

    test.py: Add entropy_global function

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import math
 import numpy
 
-def entropy(p,n,m):
+def entropy(p,n):
     return (-p/(p+n)) * math.log2(p/(p+n)) - (n/(p+n)) * math.log2(n/(p+n))
 
 # def entropy_value(p_atribute,n_atribute,p,n):
@@ -9,6 +9,22 @@ def entropy(p,n,m):
 
 #def entropy_class(class, value):
     #encontrar quantos sim e não
+
+def entropy_global(ydata):
+    possible = []
+    aux = []
+    for data in ydata:
+        if data not in possible:
+            possible.append(data)
+            aux.append(0)
+    for data in ydata:
+        count = 0
+        for value in possible:
+            if data == value:
+                aux[count] +=1
+            count +=1
+    print(aux)
+
 
 # 2º Calcular entropy para cada value de cada atributo. Ex: Sunny, Rainny, Overcast do Outlook
 
@@ -21,14 +37,13 @@ def count_p_n_global(data):
             n +=1
     return p,n
 
-data=numpy.genfromtxt("dados/weather.nominal.csv", delimiter=",", dtype=None, encoding=None)
+data=numpy.genfromtxt("dados/contact-lenses.csv", delimiter=",", dtype=None, encoding=None)
 xdata=data[1:,0:-1]
 ydata=data[1:,-1]
 attributeList=data[:1]
 
 # 1º Calcular entropia global: entropy(p,n) p = numeros de sim n = numeros de nao
-p,n = count_p_n_global(ydata)
-globalEntropy = entropy(p,n)
+entropy_global(ydata)
 
 # 2º Calcular entropy para cada value de cada atributo. Ex: Sunny, Rainny, Overcast do Outlook
 
@@ -43,7 +58,7 @@ def getValues(data, attributeList, attribute):
 def countValuePOrN(attribute, attributeList, value, xdata, ydata): # Get numbers of Positive and Negatives in a value
     countP, countN = 0,0
     attributePos = numpy.where(attributeList == attribute)
-    for x,y in zip(xdata,lydata):
+    for x,y in zip(xdata,ydata):
         if x[attributePos[1][0]] == value:
             if y == "yes":
                 countP += 1
@@ -53,6 +68,6 @@ def countValuePOrN(attribute, attributeList, value, xdata, ydata): # Get numbers
 
 
 
-values= getValues(xdata, attributeList, "outlook")
-valuePOrN = countValuePOrN("windy", attributeList, "TRUE", xdata, ydata)
-print(valuePOrN)
+# values= getValues(xdata, attributeList, "outlook")
+# valuePOrN = countValuePOrN("windy", attributeList, "TRUE", xdata, ydata)
+# print(values)
