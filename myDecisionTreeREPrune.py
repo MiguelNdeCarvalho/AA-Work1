7366d0313ff1d9de56ca8076353a703543287cca
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2020-12-29 23:14:54 +0000

    myDecisionTreeREPrune.py: Add criterion functions and all working

--- myDecisionTreeREPrune.py
+++ myDecisionTreeREPrune.py
@@ -24,10 +24,16 @@ class myDecisionTreeREPrune:
         if is_homegenus: 
             self.root = makeLeaf(homo_classe)
         else:
-            entropyGlobalValues = entropyGlobalCount(y)
-            entropyGlobal=entropyCalc(entropyGlobalValues)
+            globalTotalValues = globalTotalCount(y)
 
-            rootNode = chooseNode(self.criterion, attributeList,x,y,entropyGlobalValues, entropyGlobal) #argument
+            if self.criterion == "entropy":
+                globalTotal = entropyCalc(globalTotalValues)
+            elif self.criterion == "gini":
+                globalTotal = giniCalc(globalTotalValues)
+            elif self.criterion == "error":
+                globalTotal = errorCalc(globalTotalValues)
+
+            rootNode = chooseNode(self.criterion, attributeList,x,y,globalTotalValues, globalTotal) #argument
             #print("Root:", rootNode)
             self.root = Node()
             self.root.set_Data(rootNode)
@@ -129,15 +135,15 @@ def grow_tree(self,node,x,y,attributeList):
     print("----------------------")
     if node.get_Data()=="": #
         
-        entropyGlobalValues = entropyGlobalCount(y)
-        entropyGlobal=entropyCalc(entropyGlobalValues)
+        globalTotalValues = globalTotalCount(y)
+        globalTotal=entropyCalc(globalTotalValues)
 
-        #print(f"attributeList: {attributeList},x:{x},y:{y}, Values: {entropyGlobalValues}, Entropy: {entropyGlobal}")
-        new_branch = chooseNode(self.criterion, attributeList,x,y,entropyGlobalValues, entropyGlobal) #argument
+        #print(f"attributeList: {attributeList},x:{x},y:{y}, Values: {globalTotalValues}, Entropy: {globalTotal}")
+        new_branch = chooseNode(self.criterion, attributeList,x,y,globalTotalValues, globalTotal) #argument
         print(new_branch)
 
 
-        #print(f"Escolhe Node: {new_branch}, Y: {y}, Count: {entropyGlobalValues} , EntropyGlobal: {entropyGlobal}")
+        #print(f"Escolhe Node: {new_branch}, Y: {y}, Count: {globalTotalValues} , EntropyGlobal: {globalTotal}")
         valuesLeafs,classeLeafs = checkLeaf(new_branch, attributeList, x,y)
 
         print("valuesLeafs:",valuesLeafs,"classeLeafs:",classeLeafs)
@@ -280,7 +286,7 @@ def checkLeaf(rootNode, attributeList, xdata, ydata):
             aux_class.append(classe)
     return aux,aux_class
 
-def entropyGlobalCount(ydata):
+def globalTotalCount(ydata):
     '''
     Esta função retorna num array quantas vezes aparece cada class
     1º For - adiciona no array possible todas as classes possiveis
@@ -302,35 +308,39 @@ def entropyGlobalCount(ydata):
 
 def entropyCalc(array):
     '''
-    recebe como argumento o nmero de vezes que cada classe aparece e calcula a entropia
-    1º for - Calcula o número total de classes
-    2º for - Calcula a Entropia em si
+    recebe como argumento o numero de vezes que cada classe aparece e calcula a entropia
+    for - Calcula a Entropia em si
     '''
     #print()
-    total, result = 0,0
-    for pos in array:
-        total += pos
+    total, result = sum(array),0
     for pos in array:
         result -= (pos/total * math.log2(pos/total))
     return result
 
-"""
 def giniCalc(array):
     '''
-    recebe como argumento o nmero de vezes que cada classe aparece e calcula a entropia
-    1º for - Calcula o número total de classes
-    2º for - Calcula a Entropia em si
+    recebe como argumento o numero de vezes que cada classe aparece e calcula o gini 
+    for - Calcula o Gini em si
     '''
-    total, result = 0,0
+    total, result = sum(array),1
     for pos in array:
-        total += pos
-    for pos in array:
-        result += (pos/total * 2 * math.log2(pos/total))
+        result -= (pos/total) ** 2
     return result
-"""
 
-#entropyGlobalValues = entropyGlobalCount(ydata)
-#entropyGlobal=entropyCalc(entropyGlobalValues)
+def errorCalc(array):
+    '''
+    recebe como argumento o numero de vezes que cada classe aparece e calcula o erro
+    for - Calcula o erro em si
+    #Nao funciona bem
+    '''
+    total, aux = sum(array), []
+    for pos in array:
+        aux.append(pos/total)
+    result = 1 - max(aux) 
+    return result
+
+#globalTotalValues = globalTotalCount(ydata)
+#globalTotal=entropyCalc(globalTotalValues)
 
 # 2º Calcular entropy para cada value de cada atributo. Ex: Sunny, Rainny, Overcast do Outlook
 
@@ -355,7 +365,7 @@ def getValues(data, attributeList, attribute):
             values.append(value[attributePos[1][0]])
     return values
 
-def entropyValueCount(attribute, attributeList, value, xdata, ydata):
+def valueCount(attribute, attributeList, value, xdata, ydata):
     '''
     Função que conta o número de classes correspondentes a um value de um atributo
     1º For - adiciona no array possible todas as classes possiveis
@@ -376,7 +386,7 @@ def entropyValueCount(attribute, attributeList, value, xdata, ydata):
             count +=1
     return aux
 
-def calculateGain(attribute, attributeList, xdata, ydata, entropyGlobalValues ,entropyGlobal):
+def calculateGain(criterion,attribute, attributeList, xdata, ydata, globalTotalValues ,globalTotal):
     '''
     Função que calcula:
     1º - entropia para cada value de um atributo
@@ -387,29 +397,40 @@ def calculateGain(attribute, attributeList, xdata, ydata, entropyGlobalValues ,e
     values = getValues(xdata,attributeList, attribute)
     #print(f"Attribute: {attribute} Values: {values}")
     total = 0
-    for value in values:
-        entropyValues = entropyValueCount(attribute, attributeList, value, xdata, ydata)
-        entropy = entropyCalc(entropyValues)
-        total += (sum(entropyValues) / sum(entropyGlobalValues)) * entropy    
-    gain = entropyGlobal - total
-    return gain
 
-def chooseNode(criterion,attributeList,xdata, ydata,entropyGlobalValues, entropyGlobal):
+    if criterion == "entropy":
+        for value in values:
+            values = valueCount(attribute, attributeList, value, xdata, ydata)
+            entropy = entropyCalc(values)
+            total += (sum(values) / sum(globalTotalValues)) * entropy    
+        gain = globalTotal - total
+        return gain
+    elif criterion == "gini":
+        for value in values:
+            values = valueCount(attribute, attributeList, value, xdata, ydata)
+            gini = giniCalc(values)
+            total += (sum(values) / sum(globalTotalValues)) * gini
+        gain = globalTotal - total
+        return gain
+    elif criterion == "error":
+        for value in values:
+            values = valueCount(attribute, attributeList, value, xdata, ydata)
+            error = errorCalc(values)
+            total += (sum(values) / sum(globalTotalValues)) * error
+        gain = globalTotal - total
+        return gain
+
+def chooseNode(criterion,attributeList,xdata, ydata,globalTotalValues, globalTotal):
     '''
     Função que recebe a lista de attributes e calcula o ganho de cada e retorna o attributo com maior ganho,
     o atributo principal
     '''
     aux = []
-    if criterion == "entropy":
-        for attribute in attributeList[0]:
-            #print(f"Attribute: {attribute},Gain: {calculateGain(attribute, attributeList, xdata, ydata, entropyGlobalValues, entropyGlobal)}")
-            aux.append(calculateGain(attribute, attributeList, xdata, ydata, entropyGlobalValues, entropyGlobal))
-            index = aux.index(max(aux))
-        return attributeList[0][index]
-    if criterion == "gini":
-        pass
-    if criterion == "error":
-        pass
+    for attribute in attributeList[0]:
+        #print(f"Attribute: {attribute},Gain: {calculateGain(attribute, attributeList, xdata, ydata, globalTotalValues, globalTotal)}")
+        aux.append(calculateGain(criterion,attribute, attributeList, xdata, ydata, globalTotalValues, globalTotal))
+        index = aux.index(max(aux))
+    return attributeList[0][index]
 
 #rootNode = chooseNode(attributeList)
 # 3º Passo, ver se chegamos a um leaf(folha) e retirar
@@ -426,7 +447,7 @@ def entropyRootValues(rootNode, attributeList, valuesWithoutLeafs, xdata, ydata)
     '''
     aux=[]
     for attribute in valuesWithoutLeafs:
-        count = entropyValueCount(rootNode, attributeList, attribute, xdata, ydata)
+        count = valueCount(rootNode, attributeList, attribute, xdata, ydata)
         aux.append(entropyCalc(count))
     return aux
 
